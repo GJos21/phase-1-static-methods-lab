0186f273769cb5dbc9c2321a2ada492ca7b5387a
Greg Schmitt <gregjschmitt@gmail.com>
2021-12-15 11:08:19 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
-  //add static methods here
+  static capitalize(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+  }
+
+  static sanitize(text) {
+    return text.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(text) {
+    const alwaysLower = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "and", "from"];
+    // wanted to make this a private field but "npm update" is hanging for many minutes ...
+    return this.capitalize(text.split(" ").map(word => alwaysLower.includes(word) ? word : this.capitalize(word)).join(" "));
+  }
+
 }
